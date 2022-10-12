f637a293dcad49878cee6f6a7d51b11d6ba975ac
avivash <av@andrewvivash.com>
2022-10-12 12:58:27 -0700

    Fix: formatting

--- helpers/get-pkg-manager.ts
+++ helpers/get-pkg-manager.ts
@@ -1,31 +1,31 @@
 import { execSync } from 'child_process'
 
-export type PackageManager = 'npm' | 'pnpm' | 'yarn';
+export type PackageManager = 'npm' | 'pnpm' | 'yarn'
 
 const getPkgManager = (): PackageManager => {
   try {
-    const userAgent = process.env.npm_config_user_agent;
+    const userAgent = process.env.npm_config_user_agent
     if (userAgent) {
       if (userAgent.startsWith('npm')) {
-        return 'npm';
+        return 'npm'
       } else if (userAgent.startsWith('yarn')) {
-        return 'yarn';
+        return 'yarn'
       } else if (userAgent.startsWith('pnpm')) {
-        return 'pnpm';
+        return 'pnpm'
       }
     }
-    try {
 
-      execSync('npm --version', { stdio: 'ignore' });
-      return 'npm';
-      // execSync('yarn --version', { stdio: 'ignore' });
-      // return 'yarn';
+    try {
+      execSync('npm --version', { stdio: 'ignore' })
+      return 'npm'
+      // execSync('yarn --version', { stdio: 'ignore' })
+      // return 'yarn'
     } catch {
-      execSync('pnpm --version', { stdio: 'ignore' });
-      return 'pnpm';
+      execSync('pnpm --version', { stdio: 'ignore' })
+      return 'pnpm'
     }
   } catch {
-    return 'npm';
+    return 'npm'
   }
 }
 
